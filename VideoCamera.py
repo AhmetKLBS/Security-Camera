c757235bb05239deecaaedfb4a921986d6527787
AhmetKLBS <109668392+AhmetKLBS@users.noreply.github.com>
2022-10-10 23:56:08 +0300

    Add files via upload

--- VideoCamera.py
+++ VideoCamera.py
@@ -3,9 +3,14 @@ import numpy as np
 import video2numpy
 import time
 import tensorflow as tf
+from cvzone.FaceDetectionModule import FaceDetector
+import mediapipe as mp
 
 def VideoCamera():
-    VideoCam=cv.VideoCapture(1)
+    VideoCam=cv.VideoCapture(0)
+    fourcc=cv.VideoWriter_fourcc(*'XVID')
+    videoKayit=cv.VideoWriter('video.avi',fourcc,30.0,(640,480))
+    detector=FaceDetector()
     if VideoCam.isOpened():
         print("Kameraya Bağlanılıyor...")
     #Aşağıda while döngüsüne kadar olan iki satırda video görüntüsünün en boy oranını belirtik.
@@ -27,13 +32,22 @@ def VideoCamera():
         #bu satırı yorum satırı yapabilir.
         while True:
             ret,VideoImg=VideoCam.read()
-            if ret:
+            VideoImg,bbox= detector.findFaces(VideoImg)
+            if ret and bbox:
                 kernel = np.array([[0, -1, 0],
                                    [-1, 5, -1],
-                                   [0, -1, 0]])
+                                    [0, -1, 0]])
 
                 image_sharp = cv.filter2D(src=VideoImg, ddepth=-1, kernel=kernel)
+                center=bbox[0]["center"]
+                cv.circle(VideoImg,center,5,(255,0,255),cv.FILLED)
+                videoKayit.write(image_sharp)
                 cv.imshow("Security Camera", image_sharp)
+
+
+            elif bbox!=detector.findFaces(VideoImg):
+                time.sleep(0.5)
+                print("Yüz ALİGILANMADIĞI için kamera devredışı")
                 #changegray=cv.cvtColor(VideoImg,cv.COLOR_BGR2GRAY)     # change gray   color to    gray color_BGR2GRAY (COLOR_BGR2GRAY kameradaki görüntüyü griye çeviriyor)
             # Daha yüksek fps ve daha düşük bir yer kaplaması açısından RGB renk skalası yerine gri renk skalası tercih edilmiştir.
 #******************Bu ara kısma HDR görüntüye dönüştürme komutu ekle****************************************************
@@ -46,7 +60,8 @@ def VideoCamera():
             #*********************************************************
             else:
                 print("Hata: Çerçeve Oluşutulurken Hata Oluştu")
-            if cv.waitKey(1) & 0xFF == ord('q'):    #waitKey(1) iken bir değer bekliyor. waitKey(0) iken sonsuz oluyor.
+            exit=cv.waitKey(1)            #waitKey(1) iken bir değer bekliyor. waitKey(0) iken sonsuz oluyor.
+            if exit == 27:                #Esc tuşuna basınca kamerayı kapatıyor.
                 break
     else:
         print("Hata: Kameraya Bağlanılamadı")
