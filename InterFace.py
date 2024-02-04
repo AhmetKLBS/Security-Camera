7628272f0d5393e721046d10de6d2b9cf75e10ec
AhmetKLBS <109668392+AhmetKLBS@users.noreply.github.com>
2024-02-04 15:36:28 +0300

    InterFace.py

---
+++ InterFace.py
@@ -0,0 +1,3 @@
+import PyQt6
+#İlerleyen zamanlarda PyQt6 kütüphanesi kullanılarak güvenlik kamerası uygulamasının arayüzü tasarlanacaktır. (Tr)
+#In the future, the interface of the security camera application will be designed using the PyQt6 library. (En)
